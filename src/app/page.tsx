05ea63162c44fc8b5f61ce256bca1c4fa1305eae
adriamburgos155-cmd <adriamburgos155@gmail.com>
2026-07-08 16:48:02 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,2 +1,4 @@
-import AppShell from '@/components/AppShell'
+'use client'
+import dynamic from 'next/dynamic'
+const AppShell = dynamic(() => import('../components/AppShell'), { ssr: false })
 export default function Home() { return <AppShell /> }
